baaeedf6d00d2ee7bf448d90f69ca861203647e6
sojala <sojala@student.hive.fi>
2025-03-20 17:15:10 +0200

    Added the handling of expandables, still need to handle different quote cases

--- main.c
+++ main.c
@@ -1,15 +1,132 @@
 #include "minishell.h"
 
-void	minishell(char *input)
+/*FRIDAY TO DO:
+- quote handling (double quotes, single quotes, spaces in between - what happens when with bash??
++ REDIRS inside quotes!!*/
+
+char	*find_envp(char *exp, char **envp)
+{
+	int		i;
+	int		len;
+	char	*replacer;
+
+	i = 0;
+	len = ft_strlen(exp);
+	replacer = NULL;
+	while (envp[i])
+	{
+		if (!ft_strncmp(exp, envp[i], len))
+			replacer = ft_substr(envp[i], len + 1, ((ft_strlen(envp[i]) - len - 1)));
+		i++;
+	}
+	free (exp);
+	return (replacer);
+}
+
+char	*add_replacer(char *line, char *replacer, int k, int j)
+{
+	int		i;
+	int		l;
+	int		m;
+	int		len;
+	int		quote;
+	int		replacer_len;
+	char	*new_line;
+
+	i = 0;
+	l = 0;
+	m = 0;
+	quote = 0;
+	replacer_len = ft_strlen(replacer);
+	len = ft_strlen(line) + replacer_len;
+	new_line = malloc(len + 1);
+	if (!new_line)
+	{
+		ft_putstr_fd("minishell: memory allocation failure\n", 2);
+		exit (1);
+	}
+	while (i < (k - 1))
+		new_line[l++] = line[i++];
+	if (line[i] != '"')
+		new_line[l++] = line[i++];
+	else
+		quote = 2;
+	while (replacer_len > 0)
+	{
+		new_line[l++] = replacer[m++];
+		replacer_len--;
+	}
+	i += (j + 1 + quote);
+	while (line[i])
+		new_line[l++] = line[i++];
+	new_line[l] = '\0';
+	free (line);
+	return (new_line);
+}
+
+char	*handle_expandables(char *line, char **envp)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		quote;
+	char	*exp;
+	char	*replacer;
+
+	i = 0;
+	k = 0;
+	quote = 0;
+	while (line[i])
+	{
+		if (line[i] == '$')
+		{
+			j = 0;
+			i++;
+			k = i;
+			while (line[i] <= 'Z' && line[i] >= 'A')
+			{
+				i++;
+				j++;
+			}
+			if (line[i] == ' ' || (line[i] == '"' && quote))
+			{
+				exp = ft_substr(line, k, j);
+				if (exp && *exp)
+				{
+					replacer = find_envp(exp, envp);
+					if (replacer)
+					{
+						line = add_replacer(line, replacer, k - 1, j);
+						free (replacer);
+					}
+				}
+			}
+			i = k;
+		}
+		else
+		{
+			if (line[i] == '"')
+				quote = !quote;
+			i++;
+		}
+	}
+	return (line);
+}
+
+void	minishell(char *input, char **envp)
 {
 	t_ast	ast;
 	int		k;
 	t_node	*tmp;
 	char	*line;
+	char	*temp;
 
 	line = add_spaces(input);
 	if (!line)
 		return ;
+	temp = handle_expandables(line, envp);
+	if (temp)
+		line = temp;
 	init_sections(&ast, line);
 	init_tokens(&ast);
 	if (lexer(&ast) < 0)
@@ -30,7 +147,7 @@ void	minishell(char *input)
 	free_struct(&ast);
 }
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char **envp)
 {
 	char	*input;
 
@@ -52,7 +169,7 @@ int	main(int ac, char **av)
 		}
 		if (input)
 			add_history(input);
-		minishell(input);
+		minishell(input, envp);
 		free (input);
 	}
 	clear_history();
