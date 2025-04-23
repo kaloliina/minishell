43917ec7314f9abb50ab34d4985d7c961260ceb8
kaloliina <karol@KARO.>
2025-04-23 09:20:04 +0200

    Doing a bit of polish on the get paths .c file

--- get_paths.c
+++ get_paths.c
@@ -1,13 +1,11 @@
 #include "../minishell.h"
 
-//this one check again XD you did weird changes
 char	**get_paths(t_pipes *my_pipes)
 {
 	int		i = 0;
 	char	**paths;
 	char	*string;
 
-	string = NULL;
 	while ((*my_pipes->my_envp)[i] != NULL)
 	{
 		if (ft_strncmp((*my_pipes->my_envp)[i], "PATH=", 5) == 0)
@@ -15,29 +13,26 @@ char	**get_paths(t_pipes *my_pipes)
 			string = ft_substr((*my_pipes->my_envp)[i], 5, (ft_strlen((*my_pipes->my_envp)[i]) - 5));
 			if (string == NULL)
 				handle_fatal_exit(MALLOC, my_pipes, NULL, NULL);
-			break;
+			paths = ft_split(string, ':');
+			free (string);
+			if (paths == NULL)
+				handle_fatal_exit(MALLOC, my_pipes, NULL, NULL);
+			return (paths);
 		}
 		i++;
 	}
-	if (string == NULL)
-		return (NULL);
-	paths = ft_split(string, ':');
-	if (paths == NULL)
-	{
-		free (string);
-		handle_fatal_exit(MALLOC, my_pipes, NULL, NULL);
-	}
-	free (string);
-	return (paths);
+	return (NULL);
 }
 
 //we need to include my pipes in this because now we return NULL in case of malloc fail and if we didnt find it
+//also we need to add the check if paths is NULL, then strdup command
 char	*get_absolute_path(char **paths, char *command)
 {
-	char	*path_helper = NULL;
-	char	*path = NULL;
-	int		i = 0;
+	char	*path_helper;
+	char	*path;
+	int		i;
 
+	i = 0;
 	if (ft_strchr(command, '/'))
 	{
 		path = ft_strdup(command);
@@ -50,17 +45,11 @@ char	*get_absolute_path(char **paths, char *command)
 		if (path_helper == NULL)
 			return (NULL);
 		path = ft_strjoin(path_helper, command);
-		if (path == NULL)
-		{
-			free (path_helper);
-			return (NULL);
-		}
-		if (access(path, F_OK | X_OK) == 0)
-		{
-			free (path_helper);
-			return (path);
-		}
 		free (path_helper);
+		if (path == NULL)
+			return (NULL);
+		if (access(path, F_OK | X_OK) == 0)
+			return (path);
 		free (path);
 		i++;
 	}
