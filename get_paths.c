cd790c01e76aaf3384d446049530e195d27d9453
Karoliina Hiidenheimo <khiidenh@c2r2p11.hive.fi>
2025-04-25 12:10:58 +0300

    Shortening get_paths and fixing the bug with exit statuses.. Also shortened the finalize execution function and adjusted stuff according to norminette

--- get_paths.c
+++ get_paths.c
@@ -2,15 +2,17 @@
 
 char	**get_paths(t_pipes *my_pipes)
 {
-	int		i = 0;
+	int		i;
 	char	**paths;
 	char	*string;
 
+	i = 0;
 	while ((*my_pipes->my_envp)[i] != NULL)
 	{
 		if (ft_strncmp((*my_pipes->my_envp)[i], "PATH=", 5) == 0)
 		{
-			string = ft_substr((*my_pipes->my_envp)[i], 5, (ft_strlen((*my_pipes->my_envp)[i]) - 5));
+			string = ft_substr((*my_pipes->my_envp)[i], 5,
+					(ft_strlen((*my_pipes->my_envp)[i]) - 5));
 			if (string == NULL)
 				handle_fatal_exit(MALLOC, my_pipes, NULL, NULL);
 			paths = ft_split(string, ':');
@@ -24,15 +26,11 @@ char	**get_paths(t_pipes *my_pipes)
 	return (NULL);
 }
 
-//we need to include my pipes in this because now we return NULL in case of malloc fail and if we didnt find it
-//also we need to add the check if paths is NULL, then strdup command
 char	*get_absolute_path(char **paths, char *command, t_pipes *my_pipes)
 {
 	char	*path_helper;
 	char	*path;
-	int		i;
 
-	i = 0;
 	if (ft_strchr(command, '/') || paths == NULL)
 	{
 		path = ft_strdup(command);
@@ -40,9 +38,9 @@ char	*get_absolute_path(char **paths, char *command, t_pipes *my_pipes)
 			handle_fatal_exit(MALLOC, my_pipes, NULL, NULL);
 		return (path);
 	}
-	while (paths != NULL && paths[i] != NULL)
+	while (paths != NULL && *paths != NULL)
 	{
-		path_helper = ft_strjoin(paths[i], "/");
+		path_helper = ft_strjoin(*paths, "/");
 		if (path_helper == NULL)
 			handle_fatal_exit(MALLOC, my_pipes, NULL, NULL);
 		path = ft_strjoin(path_helper, command);
@@ -52,7 +50,7 @@ char	*get_absolute_path(char **paths, char *command, t_pipes *my_pipes)
 		if (access(path, F_OK | X_OK) == 0)
 			return (path);
 		free (path);
-		i++;
+		paths++;
 	}
 	return (NULL);
 }
